d957ea81a2dd959c635e164557a145e44f8a364b
Filip Lauc <filip.lauc93@gmail.com>
2020-02-23 16:09:56 +0100

    chore: adding @dynamic where needed

--- projects/form-builder/src/lib/field/field.component.ts
+++ projects/form-builder/src/lib/field/field.component.ts
@@ -2,6 +2,7 @@ import {Component, Inject} from '@angular/core';
 import {FieldData} from '../interfaces/field-data.interface';
 import {COMPONENT_DATA} from '../utils/create-component-injector';
 
+// @dynamic
 @Component({
   selector: 'fb-field',
   template: ''
