a89663a6d6f9d2d6a0c6aead60a5d2feaa75fe43
Filip Lauc <filip.lauc93@gmail.com>
2020-11-22 19:09:38 +0100

    feat(form-builder): parseTemplate now accepts a custom accessor method

--- projects/form-builder/src/lib/utils/parse-template.ts
+++ projects/form-builder/src/lib/utils/parse-template.ts
@@ -1,7 +1,22 @@
 export function parseTemplate(
   value = '',
-  obj: any = {}
+  obj: any = {},
+
+  /**
+   * Provide a custom method for accessing the value from the
+   * provided object
+   */
+  accessor?: (key: string, entry: any) => any
 ) {
+
+  if (!accessor) {
+    accessor = (key, entry) => key
+      .split('.')
+      .reduce((acc, cur) =>
+          acc[cur],
+        entry
+      );
+  }
 
   if (!value.includes('{{')) {
     return obj[value];
@@ -12,12 +27,7 @@ export function parseTemplate(
     while (lookUp.test(value)) {
       value = value.replace(
         lookUp,
-        (RegExp.$1)
-          .split('.')
-          .reduce((acc, cur) =>
-              acc[cur],
-            obj
-          )
+        accessor(RegExp.$1, obj)
       );
     }
 
