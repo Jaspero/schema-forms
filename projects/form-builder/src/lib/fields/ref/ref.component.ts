158653bc581bbfea0a87c68810a69a7d5395d239
AntonioStipic <antonio.stipic2@gmail.com>
2021-06-10 16:13:10 +0200

    fix(form-builder): ref imports

--- projects/form-builder/src/lib/fields/ref/ref.component.ts
+++ projects/form-builder/src/lib/fields/ref/ref.component.ts
@@ -1,10 +1,12 @@
 import {ChangeDetectionStrategy, Component, Inject, OnInit} from '@angular/core';
 import {FormControl} from '@angular/forms';
-import {COMPONENT_DATA, FieldComponent, FieldData} from '@jaspero/form-builder';
 import {Observable, of, combineLatest} from 'rxjs';
 import {map, startWith, switchMap, tap} from 'rxjs/operators';
 import {FilterMethod} from '../../enums/filter-method.enum';
+import {FieldComponent} from '../../field/field.component';
+import {FieldData} from '../../interfaces/field-data.interface';
 import {DbService} from '../../services/db.service';
+import {COMPONENT_DATA} from '../../utils/create-component-injector';
 
 interface RefData extends FieldData {
   /**
