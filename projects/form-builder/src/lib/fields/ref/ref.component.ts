9e22b26f1cf2051f5c67c0005b3b1dd82aca6a5e
AntonioStipic <antonio.stipic2@gmail.com>
2021-10-07 10:13:14 +0200

    feat(form-builder): extended ref component to support multiple selection

--- projects/form-builder/src/lib/fields/ref/ref.component.ts
+++ projects/form-builder/src/lib/fields/ref/ref.component.ts
@@ -1,14 +1,12 @@
-import {ChangeDetectionStrategy, Component, Inject, OnInit, ViewChild} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Inject, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {FormControl} from '@angular/forms';
 import {MatAutocompleteTrigger} from '@angular/material/autocomplete';
-import {BehaviorSubject, combineLatest, Observable, of} from 'rxjs';
-import {distinctUntilChanged, map, scan, startWith, switchMap, tap} from 'rxjs/operators';
+import {BehaviorSubject, combineLatest, forkJoin, Observable, of, Subscription} from 'rxjs';
+import {distinctUntilChanged, map, scan, startWith, switchMap, take, tap} from 'rxjs/operators';
+import {WhereFilter} from '@jaspero/form-builder/lib/interfaces/where-filter.interface';
+import {COMPONENT_DATA, DbService, FieldComponent, FieldData} from '@jaspero/form-builder';
+import {SelectionModel} from '@angular/cdk/collections';
 import {FilterMethod} from '../../enums/filter-method.enum';
-import {FieldComponent} from '../../field/field.component';
-import {FieldData} from '../../interfaces/field-data.interface';
-import {DbService} from '../../services/db.service';
-import {COMPONENT_DATA} from '../../utils/create-component-injector';
-import {WhereFilter} from '../../interfaces/where-filter.interface';
 
 interface RefData extends FieldData {
   /**
@@ -23,9 +21,9 @@ interface RefData extends FieldData {
   valueKey?: string;
   /**
    * Set to provided value on 'Clear' action
-   * @default null
+   * @default null, [] for multiple
    */
-  clearValue?: string | null;
+  clearValue?: string | string[] | null;
   /**
    * Max number of rows to show per search
    * @default 5
@@ -68,7 +66,7 @@ interface RefData extends FieldData {
       key: string;
       label: string;
     }[];
-  }
+  };
 
   /**
    * Additional Filters for provided collection
@@ -78,9 +76,14 @@ interface RefData extends FieldData {
 
   /**
    * Close search dialog after selecting item
-   * @default true
+   * @default true, false for multiple selection
    */
   closeOnSelect?: boolean;
+
+  /**
+   * Allow multiple selection
+   */
+  multiple?: boolean;
 }
 
 @Component({
@@ -89,32 +92,27 @@ interface RefData extends FieldData {
   styleUrls: ['./ref.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class RefComponent extends FieldComponent<RefData> implements OnInit {
+export class RefComponent extends FieldComponent<RefData> implements OnInit, OnDestroy {
+  data$: Observable<any>;
+  display$: Observable<any>;
+  displayedColumns: string[] = [];
+  searchControl: FormControl;
+  cursor = null;
+  loadMore$ = new BehaviorSubject(true);
+  lastSearch: string;
+  hasMore: boolean;
+  cleared: boolean;
+  selection: SelectionModel<any>;
+  @ViewChild(MatAutocompleteTrigger) autocomplete: MatAutocompleteTrigger;
+
+  subscriptions: Subscription[] = [];
+
   constructor(
     @Inject(COMPONENT_DATA) public cData: RefData,
     private db: DbService
   ) {
     super(cData);
   }
-
-  data$: Observable<any>;
-  display$: Observable<any>;
-
-  displayedColumns: string[] = [];
-
-  searchControl: FormControl;
-
-  cursor = null;
-
-  loadMore$ = new BehaviorSubject(true);
-
-  lastSearch: string;
-
-  hasMore: boolean;
-
-  cleared: boolean;
-
-  @ViewChild(MatAutocompleteTrigger) autocomplete: MatAutocompleteTrigger;
 
   ngOnInit() {
     this.searchControl = new FormControl('');
@@ -126,7 +124,7 @@ export class RefComponent extends FieldComponent<RefData> implements OnInit {
     this.cData.valueKey = this.cData.valueKey || 'id';
 
     if (this.cData.clearValue !== undefined) {
-      this.cData.clearValue = this.cData.clearValue ?? null;
+      this.cData.clearValue = this.cData.clearValue ?? (this.cData.multiple ? [] : null);
     }
 
     this.cData.limit = Math.abs(this.cData.limit || 4);
@@ -140,28 +138,62 @@ export class RefComponent extends FieldComponent<RefData> implements OnInit {
     this.cData.search.label = this.cData.search.label || 'Name';
     this.cData.table = this.cData.table || {};
     this.cData.table.tableColumns = this.cData.table.tableColumns || [{key: '/id', label: 'ID'}];
-    this.displayedColumns = this.cData.table.tableColumns.map(column => column.key.slice(1)) as string[];
+    this.displayedColumns = [
+      ...(this.cData.multiple ? ['select'] : []),
+      ...this.cData.table.tableColumns.map(column => column.key.slice(1)) as string[]
+    ];
 
-    this.cData.closeOnSelect = this.cData.closeOnSelect ?? true;
+    if (this.cData.multiple) {
+      this.selection = new SelectionModel(true, []);
+      forkJoin(
+        (this.cData.control.value || []).map(id => {
+          return this.db.getDocument(this.cData.collection, id);
+        })
+      ).pipe(
+        take(1)
+      ).subscribe(documents => {
+        this.selection.select(...documents);
+      });
+    }
+
+    this.cData.closeOnSelect = this.cData.closeOnSelect ?? !this.cData.multiple;
 
     this.display$ = this.cData.control.valueChanges.pipe(
-      startWith(this.cData.control.value),
+      startWith(false),
       switchMap(value => {
+
+        if (value === false) {
+          value = this.cData.control.value;
+        }
+
         if (!value) {
           return of('');
         }
 
-        return this.db.getDocument(this.cData.collection, value).pipe(
-          map((data) => {
+        return this.cData.multiple ?
+          value.length ? combineLatest(value.map(document => {
+            return this.db.getDocument(this.cData.collection, document).pipe(
+              map((data) => {
+                if (!data || String(Object.keys(data || {})) === 'id') {
+                  return value;
+                }
 
-            if (!data || String(Object.keys(data || {})) === 'id') {
-              return value;
-            }
+                // @ts-ignore
+                return data[this.cData.display.key.slice(1)] || '';
+              })
+            );
+          })) : of('')
+          : this.db.getDocument(this.cData.collection, value as any).pipe(
+            map((data) => {
 
-            // @ts-ignore
-            return data[this.cData.display.key.slice(1)] || '';
-          }),
-        );
+              if (!data || String(Object.keys(data || {})) === 'id') {
+                return value;
+              }
+
+              // @ts-ignore
+              return data[this.cData.display.key.slice(1)] || '';
+            })
+          );
       }),
       tap(display => {
         if (this.cleared) {
@@ -169,7 +201,13 @@ export class RefComponent extends FieldComponent<RefData> implements OnInit {
           return;
         }
 
-        this.searchControl.setValue(display);
+        if (!this.searchControl.value) {
+          this.searchControl.setValue('');
+        }
+
+        if (!this.cData.multiple) {
+          this.searchControl.setValue(String(display));
+        }
       })
     );
 
@@ -193,7 +231,7 @@ export class RefComponent extends FieldComponent<RefData> implements OnInit {
             // @ts-ignore
             label: this.cData.search.label,
             operator: FilterMethod.LessThen,
-            value:  search.replace(/.$/, c => String.fromCharCode(c.charCodeAt(0) + 1))
+            value: search.replace(/.$/, c => String.fromCharCode(c.charCodeAt(0) + 1))
           }
         ]);
       }),
@@ -227,7 +265,32 @@ export class RefComponent extends FieldComponent<RefData> implements OnInit {
         this.lastSearch = this.searchControl.value;
 
         return [...documents, ...(this.hasMore ? cur : [])];
-      }, [])
+      }, []),
+      map((documents) => {
+        if (!this.cData.multiple) {
+          return documents;
+        }
+
+        const selectedIds = this.selection.selected.map(item => item.id);
+        return [...this.selection.selected, ...documents.filter(document => !selectedIds.includes(document.id))]
+          .filter((document, index, arr) => arr.findIndex(item => (item.id === document.id)) === index);
+      })
+    );
+
+    this.subscriptions.push(
+      this.selection.changed.pipe(
+        tap((event) => {
+          if (this.cData.closeOnSelect) {
+            this.autocomplete.closePanel();
+          }
+
+          this.cData.control.setValue(
+            event.source.selected.map(item => {
+              return item[this.cData.valueKey];
+            })
+          );
+        })
+      ).subscribe()
     );
   }
 
@@ -262,5 +325,11 @@ export class RefComponent extends FieldComponent<RefData> implements OnInit {
 
   loadMore() {
     this.loadMore$.next(true);
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(subscription => {
+      subscription.unsubscribe();
+    });
   }
 }
